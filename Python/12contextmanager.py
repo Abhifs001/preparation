d7c4c7e0f66f8e0d90c48a14ca99750a0d158b4a
abhinav Pandey <info.pandeyabhi31@gmail.com>
2025-06-19 21:51:42 +0530

    added Python context manager example

--- Python/12contextmanager.py
+++ Python/12contextmanager.py
@@ -45,3 +45,30 @@ def connect():
 
 with connect():
     print("Work with finance data")
+
+################################################################################################
+class FileManager:
+    def __init__(self, filename, mode):
+        self.filename = filename
+        self.mode = mode
+        self.file = None
+
+    def __enter__(self):
+        print(f"Opening file: {self.filename}")
+        self.file = open(self.filename, self.mode)
+        return self.file
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if self.file:
+            self.file.close()
+            print(f"Closed file: {self.filename}")
+        # Return False to propagate exceptions (if any)
+        return False
+
+#using FileManager
+with FileManager("trades.txt", "r") as f:
+    data = f.read()
+    print(data)
+ 
+
+ 
